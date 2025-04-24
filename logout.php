339e5cbec34836d72ad42a077f4bd4b6f8181170
Chechar1999 <cesardavidriera@gmail.com>
2025-04-23 23:31:54 -0400

    logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,11 @@
 <?php
-// Iniciar la sesión para poder destruirla
+// Iniciar la sesión para poder acceder a ella
 session_start();
 
 // Destruir todas las variables de sesión.
 $_SESSION = array();
 
-// Si se desea destruir la sesión completamente, borre también la cookie de sesión.
-// Nota: ¡Esto destruirá la sesión, y no la información de la sesión!
+// Borrar la cookie de sesión si existe.
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
